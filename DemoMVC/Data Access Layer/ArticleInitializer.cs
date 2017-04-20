7f978a4cf0e1e43d3a9367ee2b728e13a2691e47
Daniel <danielguidera1@gmail.com>
2017-04-20 22:22:38 +0100

    Moved Reporting functionality from home page on each article object to the article page. Added a Modal to support this functionality.
    Added bootstrap media object inside a bootstrap well to display article thumbnail, title, blurb and source + report buttons. Source button opens original article in new tab.

--- DemoMVC/Data Access Layer/ArticleInitializer.cs
+++ DemoMVC/Data Access Layer/ArticleInitializer.cs
@@ -13,7 +13,7 @@ namespace DemoMVC.Data_Access_Layer
         {
             var articles = new List<ArticleModels>
             {
-                new ArticleModels {ID = 1, title = "God of War", thumbnail = "https://i.ytimg.com/vi/CJ_GCPaKywg/maxresdefault.jpg", blurb = "God of War blurb", link = "https://godofwar.playstation.com/" },
+                new ArticleModels {ID = 1, title = "God of War", thumbnail = "https://i.ytimg.com/vi/CJ_GCPaKywg/maxresdefault.jpg", blurb = "The latest issue of Weekly Famitsu is now available, providing our first look at Code Vein, the new Unreal Engine 4 action RPG from Bandai Namco led by key God Eater staff Keita Iizuka (producer), Hiroshi Yoshimura (director), and Yusuke Tomizawa (team leader).", link = "https://godofwar.playstation.com/" },
                 new ArticleModels {ID = 2, title = "The Last of Us", thumbnail = "https://c2.staticflickr.com/6/5597/31396458265_93118cec4a_z.jpg", blurb = "the last of us blurb", link = "https://www.naughtydog.com/blog/the_last_of_us_part_ii" },
                 new ArticleModels {ID = 3, title = "Shadow of War", thumbnail = "https://cdn.gamerant.com/wp-content/uploads/middle-earth-shadow-of-war-mithril-edition-title.jpg", blurb = "shadow of War blurb", link = "https://www.shadowofwar.com/" },
                 new ArticleModels {ID = 4, title = "Persona 5", thumbnail = "http://vignette4.wikia.nocookie.net/megamitensei/images/6/68/P5_illustration_by_Shigenori_Soejima.jpg/revision/latest?cb=20150919150530", blurb = "persona blurb", link = "http://atlus.com/persona5/"  },
